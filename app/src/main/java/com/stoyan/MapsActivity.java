1f28048ed771a75b844cd6b9374b27d0584fa89d
StoyanD <s.dimitrov19@gmail.com>
2016-08-29 15:40:47 -0500

    adding toolbar

--- app/src/main/java/com/stoyan/MapsActivity.java
+++ app/src/main/java/com/stoyan/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.stoyan;
 
+import android.databinding.DataBindingUtil;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -9,19 +10,28 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.stoyan.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    ActivityMapsBinding mBinding;
+    SupportMapFragment mMapFrag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_maps);
+
+//        mMapFrag = mBinding.;
+
+//        setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mMapFrag = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+        mMapFrag.getMapAsync(this);
     }
 
 
